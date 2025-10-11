29aa6a7aebfff2f26553d448aac1a8d234634028
“ElleChawanrat” <6632043021@student.chula.ac.th>
2025-10-11 13:24:03 +0700

    feat: add hotels controller and routes, update server setup

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser")
 const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const appointments = require("./routes/appointments");
+const hotels = require("./routes/hotels");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -20,6 +21,7 @@ app.set('query parser', 'extended');
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/v1/hotels", hotels);
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/appointments", appointments);
